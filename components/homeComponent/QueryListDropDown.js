83dff8862a65b094409c4cac6c24d383590d784c
Tonmoy Das <dev.tcdas@gmail.com>
2023-04-08 16:35:59 +0600

    UI Done

--- components/homeComponent/QueryListDropDown.js
+++ components/homeComponent/QueryListDropDown.js
@@ -7,15 +7,15 @@ import React from 'react'
 
 export default function QueryListDropDown() {
   return (
-    <Box sx={{width:'100%',padding:'15px',height:'auto',overflow:'hidden'}}>
-        <FormControl sx={{width:'50%'}}>
-            <InputLabel id="demo-simple-select-label" sx={{background:'white',marginLeft:'0px'}}>Select a Query Type&nbsp;</InputLabel>
+    <Box sx={{width:'100%',padding:'8px',height:'auto',overflow:'hidden'}}>
+        <FormControl sx={{width:'50%',height:'50px',display:'flex',justifyContent:'center'}}>
+            <InputLabel id="demo-simple-select-label" sx={{background:'white'}}>Select a Query Type &nbsp;</InputLabel>
             <Select
                 labelId="query-type-selection"
                 id="query-type-select"
                 label="query-type"
             >
-                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem  value={10}>Ten</MenuItem>
                 <MenuItem value={20}>Twenty</MenuItem> 
                 <MenuItem value={30}>Thirty</MenuItem>
             </Select>
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,53 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Box from '@mui/material/Box';
-import  Typography  from '@mui/material/Typography';
 import Menubar from '@/components/homeComponent/Menubar';
 import QueryListDropDown from '@/components/homeComponent/QueryListDropDown';
+import Editor from '@monaco-editor/react';
+import Button from '@mui/material/Button';
+import { DataGrid } from '@mui/x-data-grid';
 
-import TabPanel from '@mui/lab/TabPanel';
-import { TabContext, TabList } from '@mui/lab';
-import { Tab } from '@mui/material';
 
-const inter = Inter({ subsets: ['latin'] })
+import {useEffect, useState} from 'react';
+import { Modal } from '@mui/material';
+
+
 
 export default function Home() {
+
+  const [sparqlCode, setSparqlCode] = useState(`PREFIX qb: <http://purl.org/linked-data/cube#>
+  PREFIX qb4o: <http://purl.org/qb4olap/cubes#>
+  PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+  SELECT ?agriProductDim_Sector (COUNT(<http://www.w3.org/2001/XMLSchema#float>(?m1)) as ?area_count) 
+  WHERE {
+  ?o a qb:Observation .
+  ?o qb:dataSet <http://www.linked-agriculture-bd.com/data#agricultureForestryDataset> .
+  ?o <http://www.linked-agriculture-bd.com/mdProperty#area> ?m1 .
+  ?o <http://www.linked-agriculture-bd.com/mdProperty#Sector> ?agriProductDim_Sector .
+  }
+  GROUP BY ?agriProductDim_Sector
+  ORDER BY ?agriProductDim_Sector`);
+
+  const [open, setOpen] = useState(false);
+  const [rows, setRows] = useState([]);
+  const [columns, setColumns] = useState([
+    { field: 'id', headerName: 'ID', width: 100 },
+    { field: 'name', headerName: 'Name', width: 200 },
+    { field: 'age', headerName: 'Age', width: 100 },
+  ]);
+  const [responseTime, setResponseTime] = useState(null);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = ()=>{
+    setOpen(true)
+  }
+
+
+
+
   return (
     <>
       <Head>
@@ -24,17 +59,40 @@ export default function Home() {
       <main className={styles.main}>
           <Menubar/>
           <QueryListDropDown/>
-          <TabContext>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider', display:'flex',justifyContent:'space-around'}}>
-              <TabList aria-label="lab API tabs example" sx={{display:'flex'}}>
-                <Tab label="Queryt Editor" value="1" sx={{marginX:'20px',color:'blue',fontWeight:'bold'}}/>
-                <Tab label="Result" value="2" sx={{marginX:'20px',color:'blue',fontWeight:'bold'}}/>
-              </TabList>
-            </Box>
-            <TabPanel value="1">Item One</TabPanel>
-            <TabPanel value="2">Item Two</TabPanel>
-            <TabPanel value="3">Item Three</TabPanel>
-          </TabContext>
+          
+          <Box sx={{border:'1px solid #c2c4c2',height:'70vh',borderRadius:'8px',overflow:'hidden',marginX:'8px'}}>
+            <Editor
+                defaultLanguage="sparql"
+                defaultValue={sparqlCode}/>
+          </Box>
+          <Button sx={{backgroundColor:'#0d4d15',width:'auto',padding:'14px',margin:'auto',color:'white',borderRadius:'5px',fontWeight:'bold',border:'2px solid transparent',
+          '&:hover': 
+              {
+              border:'2px solid #0d4d15',
+              background: "white",
+              color:'#0d4d15'
+            },
+        }}
+        onClick={handleOpen}
+        
+        >Run Query</Button>
+
+
+      <Modal open={open} onClose={handleClose} sx={{display:'flex',alignItems:'center',justifyContent:'center'}}>
+        <div style={{ width: '95vw', height: '95vh', backgroundColor: 'white', margin: 'auto', padding: '10px', borderRadius: '5px', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',overflow:'auto' }}>
+          <h3 style={{marginBottom:'10px'}}>Query Result ({responseTime ? `${responseTime} ms` : '-'})</h3>
+          <div style={{ height: '80vh', width: '100%' }}>
+            <DataGrid rows={rows} columns={columns} pageSize={10} />
+          </div>
+          <Box sx={{display:'flex',justifyContent:'center'}}>
+            <Button variant="contained" color="primary" onClick={handleClose} style={{marginTop: '7px' }}>
+              Close
+            </Button>
+          </Box>
+          
+        </div>
+      </Modal>
+          
       </main>
     </>
   )
